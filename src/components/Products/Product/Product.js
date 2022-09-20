48c5a9ed7831953bdf5c158521a5ac8f1279e96f
Hadar4476 <hadar4476@gmail.com>
2022-09-20 17:30:16 +0300

    Initialized cart items, added functionality for adding/increasing a product and its quantity.

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { globalActions } from "../../../store";
 
 const Product = (props) => {
   const { id, title, description, image, price } = props;
+
+  const { items } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const onIncreaseQuantity = () => {
+    const product = {
+      id,
+      title,
+      description,
+      image,
+      price,
+    };
+
+    dispatch(globalActions.cart.increaseQuantity(product));
+  };
 
   return (
     <div>
@@ -13,7 +30,7 @@ const Product = (props) => {
       <div>
         <div>
           <button>-</button>
-          <button>+</button>
+          <button onClick={onIncreaseQuantity}>+</button>
         </div>
         <div>
           <span>{price}</span>
--- src/store/slices/cart.js
+++ src/store/slices/cart.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   shouldDisplay: false,
+  items: [],
 };
 
 const showCart = (state) => {
@@ -12,12 +13,25 @@ const hideCart = (state) => {
   state.shouldDisplay = false;
 };
 
+const increaseQuantity = (state, action) => {
+  const product = action.payload;
+  const productInCart = state.items.findIndex((i) => i.id === product.id);
+
+  if (productInCart < 0) {
+    product.quantity = 1;
+    state.items.push(product);
+  } else {
+    state.items[productInCart].quantity++;
+  }
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     showCart,
     hideCart,
+    increaseQuantity,
   },
 });
 
